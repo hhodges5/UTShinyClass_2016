5234317aac7d68127a6cdc4a05dd91506508966c
Mark Ewing <pofigster@gmail.com>
2016-10-02 20:47:21 -0400

    folder restructure
    renamed folders to make flow more obvious

---
+++ app walkthrough.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+# Empty Dashboard
+runApp("01 - empty dashboard/")
+
+# Basic IO
+runApp("02 - basic io dashboard/")
+
+# Basic Reactivity
+runApp("03 - basic reactivity/")
+
+# Fancy Outputs
+runApp("04 - fancy outputs/")
